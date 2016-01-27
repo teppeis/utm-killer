4293b889d99031a2afab7b4b13e67e29387c4789
teppeis <teppeis@gmail.com>
2016-01-27 22:55:30 +0900

    feat(background): implement utm killer

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,4 +4,35 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-console.log('\'Allo \'Allo! Event Page');
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (!changeInfo.status) {
+    // only favIconUrl is changed
+    return;
+  }
+
+  let queryStringIndex = tab.url.indexOf('?');
+  if (tab.url.indexOf('utm_') > queryStringIndex) {
+    let stripped = tab.url.replace(
+      /([\?\&]utm_(source|medium|term|campaign|content|cid|reader)=[^&#]+)/ig,
+      ''
+    );
+    if (stripped.charAt(queryStringIndex) === '&') {
+      stripped = stripped.substr(0, queryStringIndex) + '?' +
+        stripped.substr(queryStringIndex + 1);
+    }
+    if (stripped !== tab.url) {
+      if (/'/.test(stripped)) {
+        console.error('invalid url', stripped);
+        return;
+      }
+      let code = `window.location.replace('${stripped}')`;
+      let details = {
+        code,
+        runAt: 'document_start'
+      };
+      chrome.tabs.executeScript(tab.id, details, (result) => {
+        console.log('replaced', stripped, result);
+      });
+    }
+  }
+});
